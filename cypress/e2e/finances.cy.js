cfeaf6cdc84ae0f236a5c8c5442791563f02ac4a
Damnpedrini <damnpedrini@outlook.com>
2023-02-14 13:06:24 -0300

    add: new test finances

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -5,13 +5,55 @@ context('Dev Finances Agilizei', () => {
 
         cy.visit('https://dev-finances-smoky.vercel.app/')
 
-        cy.get('#transaction .button').click() //id + classe
-        cy.get('#description').type('Mesada') //id
-        cy.get('[name=amount]').type(15) //atributos
-        cy.get('[type=date]').type('1999-12-31') //atributos
-        cy.get('button').contains('Salvar').click() // tipo e valor
+        cy.get('#transaction .button')
+            .should("be.visible") //ver visibilidade
+            .click() //id + classe
+        cy.get('#description')
+            .clear() //Limpar imput
+            .should("have.value", "")
+            .type('Mesada') //id
+            .should("have.value", "Mesada")
+           
+        cy.get('[name=amount]')
+            .type(15) //atributos
+            .should("have.value", "15")
+        cy.get('[type=date]')
+            .type('1999-12-31') //atributos
+            .should("have.value","1999-12-31" )
+            
+
+        cy.get('button').contains('Salvar')
+            .click() // tipo e valor
 
         cy.get('#data-table tbody tr').should('have.length', 1)  //dados cadastrados
 });
+
+        it('Editar Entradas', () => {
+            cy.visit('https://dev-finances-smoky.vercel.app/')
+
+        cy.get('#transaction .button')
+            .should("be.visible") //ver visibilidade
+            .click() //id + classe
+        cy.get('#description')
+            .clear() //Limpar imput
+            .should("have.value", "")
+            .type('Mesada') //id
+            .should("have.value", "Mesada")
+            .clear() //Limpar imput
+
+        cy.get('[name=amount]')
+            .type(0) //atributos
+            .should("have.value", "")
+        cy.get('[type=date]')
+            .type('1999-12-31') //atributos
+            .should("have.value","1999-12-31" )
+            
+
+        cy.get('button').contains('Salvar')
+            .click() // tipo e valor
+
+        cy.get('#data-table tbody tr').should('have.length', 0)  //dados cadastrados
+
+        });
     
 });
